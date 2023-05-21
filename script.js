6210d79324da3b2e915fa3236d60a3cef65be14e
JoanSc <joanseco5@gmail.com>
2023-05-20 20:27:17 -0500

    Añadi comentarios respectivos al codigo

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
+// Obtiene la referencia al botón, al mensaje de error y a la galería en el HTML
 const btnEl = document.getElementById("btn");
 const errorMessageEl = document.getElementById("errorMessage");
 const galleryEl = document.getElementById("gallery");
 
+// Función asincrónica que realiza la solicitud de imágenes
 async function fetchImage() {
   const inputValue = document.getElementById("input").value;
 
+  // Verifica si el valor ingresado está fuera del rango permitido (1-10)
   if (inputValue > 10 || inputValue < 1) {
     errorMessageEl.style.display = "block";
     errorMessageEl.innerText = "Los números deben estar entre 1 y 10";
@@ -15,8 +18,10 @@ async function fetchImage() {
 
   try {
     btnEl.style.display = "none";
+    // Muestra un ícono de carga mientras se realizan las peticiones
     const loading = `<img src="spinner.svg" />`;
     galleryEl.innerHTML = loading;
+    // Realiza una petición a la API de Unsplash para obtener las imágenes
     await fetch(
       `https://api.unsplash.com/photos?per_page=${inputValue}&page=${Math.round(
         Math.random() * 1000
@@ -24,6 +29,7 @@ async function fetchImage() {
     ).then((res) =>
       res.json().then((data) => {
         if (data) {
+          // Genera el Div HTML para cada imagen recibida y las agrega al elemento de la galería
           data.forEach((pic) => {
             imgs += `
             <img src=${pic.urls.small} alt="image"/>`;
@@ -37,11 +43,13 @@ async function fetchImage() {
     );
   } catch (error) {
     console.log(error);
+    // Muestra un mensaje de error si hay algún problema con la petición
+    errorMessageEl.style.display = "block";
     errorMessageEl.style.display = "block";
     errorMessageEl.innerHTML = "El número de peticiones está agotado, trata mas tarde";
     btnEl.style.display = "block";
     galleryEl.style.display = "none";
   }
 }
-
+// Agrega un event listener al botón para ejecutar la función fetchImage al hacer clic
 btnEl.addEventListener("click", fetchImage);
